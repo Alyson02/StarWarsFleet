7568e56a368b180899f7465e2296b435a25ae4fc
alyson02 <alyson.subrim2002@gmail.com>
2025-04-12 02:31:37 -0300

    feat: implement faction controller

---
+++ StarWarsFleet.Domain/Models/FactionModel.cs
@@ -0,0 +1,6 @@
+﻿namespace StarWarsFleet.Domain.Models;
+
+public class FactionViewModel
+{
+    public string Name { get; set; } = String.Empty;
+}
---
+++ StarWarsFleet.Web/Controllers/FactionController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StarWarsFleet.Domain.Entities;
+using StarWarsFleet.Domain.Models;
+using StarWarsFleet.Infrastructure.Data;
+
+namespace StarWarsFleet.Web.Controllers;
+
+[Route("faction")]
+public class FactionController(StarWarsDbContext context) : Controller
+{
+    [HttpPost]
+    public async Task<IActionResult> AddFaction([FromBody] FactionViewModel model)
+    {
+        var faction = new FactionEntity { Id = Guid.NewGuid(), Name = model.Name };
+        await context.Factions.AddAsync(faction);
+        await context.SaveChangesAsync();
+        return Ok(faction);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> ListFactions()
+    {
+        return Ok(await context.Factions.ToListAsync());
+    }
+    
+    [HttpDelete("{factionId}")]
+    public async Task<IActionResult> DeleteFaction([FromRoute] string factionId)
+    {
+        var faction = await GetFaction(factionId);
+        
+        if (faction is null) return NotFound();
+        
+        context.Factions.Remove(faction);
+        await context.SaveChangesAsync();
+        
+        return Ok();
+    }
+
+    [HttpPut("{factionId}")]
+    public async Task<IActionResult> DeleteFaction([FromRoute] string factionId, [FromBody] FactionViewModel model)
+    {
+        var faction = await GetFaction(factionId);
+
+        if (faction is null) return NotFound();
+
+        faction.Name = model.Name;
+        
+        context.Factions.Update(faction);
+        await context.SaveChangesAsync();
+        
+        return Ok(faction);
+    }
+    
+    private async Task<FactionEntity?> GetFaction(string factionId)
+    {
+        var faction = await context.Factions.FirstOrDefaultAsync(x => x.Id.ToString() == factionId);
+        return faction;
+    }
+}
