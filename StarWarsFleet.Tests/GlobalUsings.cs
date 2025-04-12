3fa78c348115f6c892a27b1bbca27ed23f3fd8fd
alyson02 <alyson.subrim2002@gmail.com>
2025-04-12 03:44:11 -0300

    feat: adiciona testes

---
+++ StarWarsFleet.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using StarWarsFleet.Domain.Entities;
+global using StarWarsFleet.Domain.Models;
+global using StarWarsFleet.Infrastructure.Data;
+global using Microsoft.EntityFrameworkCore.InMemory;
